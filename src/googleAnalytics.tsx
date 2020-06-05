bcab24c01e5efa81cef57f3469270fa10cbb7d06
Jeremy <jetsai@hmc.edu>
2020-06-05 10:50:14 -0700

    Turn on test mode for ReactGA

--- src/googleAnalytics.tsx
+++ src/googleAnalytics.tsx
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga"
  
 export const initGA = () => {
-  ReactGA.initialize('UA-168532476-1');
+  ReactGA.initialize('UA-168532476-1', {testMode: true});
 }
  
 export const logPageView = () => {
