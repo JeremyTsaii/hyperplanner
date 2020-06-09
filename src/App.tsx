c84c06fac04a3bdf910d634cd07112795469873b
Jeremy <jetsai@hmc.edu>
2020-06-09 16:23:25 -0700

    Add github and login buttons/icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline'
 import Layout from './layout'
 import logo from './logo.svg'
 import './App.css'
@@ -10,9 +13,40 @@ function App(): JSX.Element {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>The beginning of HyperPlanner (coming soon)!</p>
+          <LoginButton />
+          <GitButton />
         </header>
       </div>
     </Layout>
+  )
+}
+
+function LoginButton(): JSX.Element {
+  return (
+    <Button
+      variant="outlined"
+      color="primary"
+      startIcon={<PersonOutlineIcon />}>
+      Login
+    </Button>
+  )
+}
+
+function GitButton(): JSX.Element {
+  return (
+    <Button
+      variant="outlined"
+      color="secondary"
+      startIcon={<GitHubIcon />}
+      onClick={() =>
+        window.open(
+          'https://github.com/JeremyTsaii/hyperplanner',
+          '_blank',
+          'noopener, noreferrer',
+        )
+      }>
+      GitHub
+    </Button>
   )
 }
 
