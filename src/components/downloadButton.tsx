944dabcc7051c3929dc3f31a275c425389f748d5
Jeremy <jetsai@hmc.edu>
2020-06-11 01:03:49 -0700

    Change icon to outlined icon

--- src/components/downloadButton.tsx
+++ src/components/downloadButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import GetAppIcon from '@material-ui/icons/GetApp'
+import GetAppOutlinedIcon from '@material-ui/icons/GetAppOutlined'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -18,7 +18,7 @@ function DownloadButton(): JSX.Element {
       variant="outlined"
       color="secondary"
       className={classes.downloadButton}
-      startIcon={<GetAppIcon />}>
+      startIcon={<GetAppOutlinedIcon />}>
       Download
     </Button>
   )
