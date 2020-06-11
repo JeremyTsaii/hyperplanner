b35523853b613405b47194b27d9339d5d8d2c549
Jeremy <jetsai@hmc.edu>
2020-06-11 12:18:50 -0700

    Remove unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core/styles'
 import Layout from './components/layout'
 import './App.css'
 import TopBar from './components/topBar'
 import Info from './components/info'
 import Cards from './components/cards'
 
-const useStyles = makeStyles(() => ({}))
-
 function App(): JSX.Element {
-  const classes = useStyles()
-
   return (
     <Layout>
       <div className="App">
