a1f3100d848a8a0d468371078012429a49ca4398
Jeremy <jetsai@hmc.edu>
2020-06-10 21:12:16 -0700

    Remove theme for lint

--- src/components/gitButton.tsx
+++ src/components/gitButton.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import GitHubIcon from '@material-ui/icons/GitHub'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  gitButton: {
+    marginRight: theme.spacing(1),
+  },
+}))
 
 function GitButton(): JSX.Element {
+  const classes = useStyles()
+
   return (
     <Button
       variant="outlined"
       color="secondary"
+      className={classes.gitButton}
       startIcon={<GitHubIcon />}
       onClick={() =>
         window.open(
