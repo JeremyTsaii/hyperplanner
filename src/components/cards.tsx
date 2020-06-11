c7af4b9886cb5ca4e6d379f5683fce47650cbdd1
Jeremy <jetsai@hmc.edu>
2020-06-10 21:13:27 -0700

    Remove theme for lint

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -3,7 +3,7 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   grid: {
     background: '#282c34',
   },
--- src/components/info.tsx
+++ src/components/info.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   grid: {
     background: '#282c34',
   },
