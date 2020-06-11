5790a6187729d18e0a3329dc5493f23273504bc1
Jeremy <jetsai@hmc.edu>
2020-06-11 00:58:09 -0700

    Fix text transform

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles((theme) => ({
   loginButton: {
     marginRight: theme.spacing(1),
+    textTransform: 'none',
   },
 }))
 
