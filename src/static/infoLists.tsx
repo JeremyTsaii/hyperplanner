ff0bcbad6789f0892e6e9a8c5748a77357a1f307
Jeremy <jetsai@hmc.edu>
2020-06-24 12:00:05 -0700

    Add dropdown lists

---
+++ src/static/infoLists.tsx
@@ -0,0 +1,207 @@
+export const schools = [
+  {
+    value: 'hmc',
+    label: 'Harvey Mudd College',
+  },
+]
+
+export const majors = [
+  {
+    value: 'bio',
+    label: 'Biology',
+  },
+  {
+    value: 'bio_chem',
+    label: 'Joint Biology/Chemistry',
+  },
+  {
+    value: 'chem',
+    label: 'Chemistry',
+  },
+  {
+    value: 'cs',
+    label: 'Computer Science',
+  },
+  {
+    value: 'cs_math',
+    label: 'Joint Computer Science/Mathematics',
+  },
+  {
+    value: 'engr',
+    label: 'Engineering',
+  },
+  {
+    value: 'math',
+    label: 'Mathematics',
+  },
+  {
+    value: 'math_phys',
+    label: 'Joint Mathematics/Physics',
+  },
+  {
+    value: 'mcb',
+    label: 'Mathematical/Computational Biology',
+  },
+  {
+    value: 'phys',
+    label: 'Physics',
+  },
+]
+
+export const gradYears = [
+  {
+    value: '2021',
+  },
+  {
+    value: '2022',
+  },
+  {
+    value: '2023',
+  },
+  {
+    value: '2024',
+  },
+  {
+    value: '2025',
+  },
+  {
+    value: '2026',
+  },
+]
+
+export const concentrations = [
+  {
+    value: 'American Studies',
+  },
+  {
+    value: 'Anthropology',
+  },
+  {
+    value: 'Art',
+  },
+  {
+    value: 'Art History',
+  },
+  {
+    value: 'Asian American Studies',
+  },
+  {
+    value: 'Asian Studies',
+  },
+  {
+    value: 'Africana Studies',
+  },
+  {
+    value: 'Chicanx/Latinx Studies',
+  },
+  {
+    value: 'Classics',
+  },
+  {
+    value: 'Dance',
+  },
+  {
+    value: 'Economics',
+  },
+  {
+    value: 'Education',
+  },
+  {
+    value: 'Environmental Analysis',
+  },
+  {
+    value: 'European Studies',
+  },
+  {
+    value: 'Foreign Languages',
+  },
+  {
+    value: 'Gender Studies',
+  },
+  {
+    value: 'Geography',
+  },
+  {
+    value: 'German Studies',
+  },
+  {
+    value: 'History',
+  },
+  {
+    value: 'Holocaust & Human Rights',
+  },
+  {
+    value: 'Jewish Studies',
+  },
+  {
+    value: 'Latin American Studies',
+  },
+  {
+    value: 'Linguistics',
+  },
+  {
+    value: 'Literature',
+  },
+  {
+    value: 'Media Studies',
+  },
+  {
+    value: 'Middle Eastern Studies',
+  },
+  {
+    value: 'Music',
+  },
+  {
+    value: 'Philosophy',
+  },
+  {
+    value: 'Politcal Studies',
+  },
+  {
+    value: 'Psychology',
+  },
+  {
+    value: 'Public Policy Analysis',
+  },
+  {
+    value: 'Religious Studies',
+  },
+  {
+    value: 'Science, Technology, & Society',
+  },
+  {
+    value: 'Secular Studies',
+  },
+  {
+    value: 'Sociology',
+  },
+  {
+    value: 'Theatre',
+  },
+  {
+    value: 'Writing and Rhetoric',
+  },
+]
+
+export const campuses = [
+  {
+    value: 'hmc',
+    label: 'Harvey Mudd College',
+  },
+  {
+    value: 'sc',
+    label: 'Scripps College',
+  },
+  {
+    value: 'po',
+    label: 'Pomona College',
+  },
+  {
+    value: 'cmc',
+    label: 'Claremont McKenna College',
+  },
+  {
+    value: 'pz',
+    label: 'Pitzer College',
+  },
+]
