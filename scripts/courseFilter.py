9cafc46ca500d4b40110f623b6352b4e4662106a
Restyled.io <commits@restyled.io>
2020-12-09 19:24:45 +0000

    Restyled by black

--- scripts/courseFilter.py
+++ scripts/courseFilter.py
@@ -3,12 +3,12 @@ import json
 
 def courseFilter():
     # Load courses from json file
-    with open('../src/static/coursesRaw.json') as f:
+    with open("../src/static/coursesRaw.json") as f:
         courses = json.load(f)
     f.close()
 
     # Sort by course code
-    courses.sort(key=lambda x: x['code'])
+    courses.sort(key=lambda x: x["code"])
 
     filteredCourses = []
     c = (0, 0, 0, 0)
@@ -16,17 +16,20 @@ def courseFilter():
 
     for course in courses:
         # Read next course
-        newC = (course['code'], course['title'],
-                course['campus'], course['credits'])
-        if newC[0] == c[0] and newC[2] == 'hmc':
+        newC = (course["code"], course["title"], course["campus"], course["credits"])
+        if newC[0] == c[0] and newC[2] == "hmc":
             # If course duplicate, keep the HMC course
 
             c = newC
         elif newC[0] != c[0] and c[0] != 0:
             # If newC is a new, valid course, add new course to fildterCourses
             if not any(i.islower() for i in str(c[0])):
-                newCourse = {'code': c[0], 'title': c[1],
-                             'campus': c[2], 'credits': c[3]}
+                newCourse = {
+                    "code": c[0],
+                    "title": c[1],
+                    "campus": c[2],
+                    "credits": c[3],
+                }
                 filteredCourses.append(newCourse)
             c = newC
         elif c[0] == 0:
@@ -34,12 +37,11 @@ def courseFilter():
 
     # Add in the last course if valid
     if not any(i.islower() for i in str(c[0])):
-        newCourse = {'code': c[0], 'title': c[1],
-                     'campus': c[2], 'credits': c[3]}
+        newCourse = {"code": c[0], "title": c[1], "campus": c[2], "credits": c[3]}
         filteredCourses.append(newCourse)
 
     # Write update allCourses.json with the filtered courses
-    newJson = open('../src/static/allCourses.json', 'w')
+    newJson = open("../src/static/allCourses.json", "w")
     json.dump(filteredCourses, newJson, indent=2)
 
 
