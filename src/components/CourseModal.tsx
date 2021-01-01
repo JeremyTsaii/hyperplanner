2d1c8f9a2611b654d896a516d34288ef816dc875
godpng <jayoo@g.hmc.edu>
2020-12-31 16:38:40 -0800

    Update codegen, remove major/core checks column

--- src/components/CourseModal.tsx
+++ src/components/CourseModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { Typography } from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton'
 import AddIcon from '@material-ui/icons/Add'
@@ -12,7 +12,6 @@ import MenuItem from '@material-ui/core/MenuItem'
 import Button from '@material-ui/core/Button'
 import { makeStyles, withStyles } from '@material-ui/core/styles'
 import { Autocomplete } from '@material-ui/lab'
-import { modifyChecklist } from '../utils/modalFunctions'
 import AllCourses from '../static/allCourses.json'
 import {
   types,
@@ -24,17 +23,12 @@ import {
 /* eslint-disable */
 import {
   useAdd_CourseMutation,
-  useUpdate_Major_ChecksMutation,
-  useUpdate_Core_ChecksMutation,
   useIncrement_Course_EditsMutation,
-  Get_InfoDocument,
-  Get_InfoQuery,
   Get_CoursesQuery,
   Get_CoursesDocument,
   Courses,
 } from '../generated/graphql'
 /* eslint-enable */
-import { UserContext } from '../context/UserContext'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -101,10 +95,6 @@ const DialogActions = withStyles((theme) => ({
 function CourseModal({ functional, term, year }: DialogProps): JSX.Element {
   const [updateCourseEdits] = useIncrement_Course_EditsMutation()
   const [addCourse] = useAdd_CourseMutation()
-  const [updateMajorChecks] = useUpdate_Major_ChecksMutation()
-  const [updateCoreChecks] = useUpdate_Core_ChecksMutation()
-
-  const { data: infoData } = useContext(UserContext)
 
   // Changing information in modal
   const [campus, setCampus] = useState('')
@@ -177,9 +167,6 @@ function CourseModal({ functional, term, year }: DialogProps): JSX.Element {
     )
   }
   const handleSave = () => {
-    const info = infoData.users[0]
-    const { majorChecks, coreChecks, school, auth0_id: id } = info
-
     const newTitle = getValue(titleRef)
     const newCode = getValue(codeRef)
     const formatTerm = term.toLowerCase() + year
@@ -240,80 +227,6 @@ function CourseModal({ functional, term, year }: DialogProps): JSX.Element {
                 campus,
                 writ_inten: writInten === 'True',
                 active: true,
-              },
-            ],
-          },
-        },
-      })
-
-      // Update checklists
-      const [newMajorChecks, newCoreChecks] = modifyChecklist(
-        newCode,
-        1,
-        majorChecks,
-        coreChecks,
-        school,
-      )
-      updateMajorChecks({
-        variables: {
-          id,
-          majorChecks: newMajorChecks,
-        },
-        update(cache) {
-          /* eslint-disable */
-          const existingInfo = cache.readQuery<Get_InfoQuery>({
-            query: Get_InfoDocument,
-          })
-          const newInfo = { ...existingInfo!.users[0] }
-          newInfo.majorChecks = newMajorChecks
-          cache.writeQuery<Get_InfoQuery>({
-            query: Get_InfoDocument,
-            data: { users: [newInfo] },
-          })
-          /* eslint-enable */
-        },
-        optimisticResponse: {
-          __typename: 'mutation_root',
-          update_users: {
-            __typename: 'users_mutation_response',
-            affected_rows: 1,
-            returning: [
-              {
-                __typename: 'users',
-                majorChecks: newMajorChecks,
-              },
-            ],
-          },
-        },
-      })
-
-      updateCoreChecks({
-        variables: {
-          id,
-          coreChecks: newCoreChecks,
-        },
-        update(cache) {
-          /* eslint-disable */
-          const existingInfo = cache.readQuery<Get_InfoQuery>({
-            query: Get_InfoDocument,
-          })
-          const newInfo = { ...existingInfo!.users[0] }
-          newInfo.coreChecks = newCoreChecks
-          cache.writeQuery<Get_InfoQuery>({
-            query: Get_InfoDocument,
-            data: { users: [newInfo] },
-          })
-          /* eslint-enable */
-        },
-        optimisticResponse: {
-          __typename: 'mutation_root',
-          update_users: {
-            __typename: 'users_mutation_response',
-            affected_rows: 1,
-            returning: [
-              {
-                __typename: 'users',
-                coreChecks: newCoreChecks,
               },
             ],
           },
--- src/components/DeleteIcon.tsx
+++ src/components/DeleteIcon.tsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
-import { modifyChecklist } from '../utils/modalFunctions'
 /* eslint-disable */
 import {
   useRemove_CourseMutation,
-  useUpdate_Major_ChecksMutation,
-  useUpdate_Core_ChecksMutation,
   useIncrement_Course_EditsMutation,
   Get_InfoDocument,
   Get_InfoQuery,
@@ -15,36 +12,22 @@ import {
   Get_CoursesDocument,
 } from '../generated/graphql'
 /* eslint-enable */
-import { UserContext } from '../context/UserContext'
 
 interface deleteProps {
   functional: boolean
   term: string
   title: string
-  code: string
 }
 
-function DeleteIcon({
-  functional,
-  term,
-  title,
-  code,
-}: deleteProps): JSX.Element {
+function DeleteIcon({ functional, term, title }: deleteProps): JSX.Element {
   const [courseRemove] = useRemove_CourseMutation()
   const [updateCourseEdits] = useIncrement_Course_EditsMutation()
-  const [updateMajorChecks] = useUpdate_Major_ChecksMutation()
-  const [updateCoreChecks] = useUpdate_Core_ChecksMutation()
-
-  const { data: infoData } = useContext(UserContext)
 
   // Delete course on icon click
   // If not functional, clicking does nothing (user logged out)
   const handleDelete = !functional
     ? undefined
     : () => {
-        const info = infoData.users[0]
-        const { majorChecks, coreChecks, school, auth0_id: id } = info
-
         // Update user course_edits column
         updateCourseEdits()
 
@@ -70,81 +53,6 @@ function DeleteIcon({
             delete_courses: {
               __typename: 'courses_mutation_response',
               affected_rows: 1,
-            },
-          },
-        })
-
-        // Update checklists
-        const [newMajorChecks, newCoreChecks] = modifyChecklist(
-          code,
-          0,
-          majorChecks,
-          coreChecks,
-          school,
-        )
-
-        updateMajorChecks({
-          variables: {
-            id,
-            majorChecks: newMajorChecks,
-          },
-          update(cache) {
-            /* eslint-disable */
-            const existingInfo = cache.readQuery<Get_InfoQuery>({
-              query: Get_InfoDocument,
-            })
-            const newInfo = { ...existingInfo!.users[0] }
-            newInfo.majorChecks = newMajorChecks
-            cache.writeQuery<Get_InfoQuery>({
-              query: Get_InfoDocument,
-              data: { users: [newInfo] },
-            })
-            /* eslint-enable */
-          },
-          optimisticResponse: {
-            __typename: 'mutation_root',
-            update_users: {
-              __typename: 'users_mutation_response',
-              affected_rows: 1,
-              returning: [
-                {
-                  __typename: 'users',
-                  majorChecks: newMajorChecks,
-                },
-              ],
-            },
-          },
-        })
-
-        updateCoreChecks({
-          variables: {
-            id,
-            coreChecks: newCoreChecks,
-          },
-          update(cache) {
-            /* eslint-disable */
-            const existingInfo = cache.readQuery<Get_InfoQuery>({
-              query: Get_InfoDocument,
-            })
-            const newInfo = { ...existingInfo!.users[0] }
-            newInfo.coreChecks = newCoreChecks
-            cache.writeQuery<Get_InfoQuery>({
-              query: Get_InfoDocument,
-              data: { users: [newInfo] },
-            })
-            /* eslint-enable */
-          },
-          optimisticResponse: {
-            __typename: 'mutation_root',
-            update_users: {
-              __typename: 'users_mutation_response',
-              affected_rows: 1,
-              returning: [
-                {
-                  __typename: 'users',
-                  coreChecks: newCoreChecks,
-                },
-              ],
             },
           },
         })
